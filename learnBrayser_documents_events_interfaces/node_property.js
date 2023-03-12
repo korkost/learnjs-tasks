f91a71ab92ae70c277a2abee10ca3daa1693c11f
korkost <koreikinvk@gmail.com>
2023-03-12 11:49:08 +0300

    пройдено 6 тем DOM

---
+++ learnBrayser_documents_events_interfaces/node_property.js
@@ -0,0 +1,18 @@
+// for (let li of document.querySelectorAll('li')) {
+//     let title = li.firstChild.data;
+// }
+
+
+
+
+// let body = document.body;
+
+// body.innerHTML = "<!--" + body.tagName + "-->";
+
+// console.log(body.firstChild.data);
+
+
+
+console.log(HTMLDocument.prototype.constructor.name);
+console.log(HTMLDocument.prototype.__proto__.constructor.name);
+console.log(HTMLDocument.prototype.__proto__.__proto__.constructor.name);
